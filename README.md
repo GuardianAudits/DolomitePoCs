d8a596d0d6c5f872931e45f7e6870617c251a4b0
GuardianAudits <guardiansolidity@gmail.com>
2023-11-01 22:20:03 -0400

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+<p align="center">
+<img src="./dolomite-logo.png" width="256" />
+</p>
+
+<div align="center">
+  <a href='https://coveralls.io/github/dolomite-exchange/dolomite-margin-modules?branch=master'>
+    <img src='https://coveralls.io/repos/github/dolomite-exchange/dolomite-margin-modules/badge.svg?branch=master&longCache=false' alt='Coverage Status' />
+  </a>
+  <a href='https://github.com/dolomite-exchange/dolomite-margin-modules/blob/master/LICENSE' style="text-decoration:none;">
+    <img src='https://img.shields.io/badge/GPL--3.0-llicense-red?longCache=true' alt='License' />
+  </a>
+  <a href='https://discord.com/invite/uDRzrB2YgP' style="text-decoration:none;">
+    <img src='https://img.shields.io/badge/chat-on%20discord-7289DA.svg?longCache=true' alt='Discord' />
+  </a>
+  <a href='https://t.me/dolomite_official' style="text-decoration:none;">
+    <img src='https://img.shields.io/badge/chat-on%20telegram-9cf.svg?longCache=true' alt='Telegram' />
+  </a>
+  <a href='https://github.com/dolomite-exchange/dolomite-margin-modules' style="text-decoration:none;">
+    <img src='https://img.shields.io/badge/GitHub-dolomite--exchange%2Fdolomite--margin--modules-lightgrey' alt='GitHub'/>
+  </a>
+</div>
+
+> Solidity Smart Contracts for integration tests against the live `DolomiteMargin` instance on all deployed networks for
+> adding new modules that hook into a live deployment.
+
+## Installation
+
+1. Run `yarn install` to install the rest of the dependencies.
+2. Sign up on [Infura](https://infura.io/register) or [Alchemy](https://www.alchemy.com/). We recommend using Infura or
+   Alchemy to allow for a reproducible Arbitrum One testing environment as well as efficiency due to caching.
+3. Run `yarn build` to compile all smart contracts and build TypeChain wrappers
+
+## Testing
+
+1. To run all tests, run `yarn test`
---
+++ src/utils/index.ts
@@ -0,0 +1,80 @@
+import { AmountDenomination, AmountReference } from '@dolomite-margin/dist/src';
+import { BigNumber, BigNumberish, BytesLike } from 'ethers';
+import { ethers } from 'hardhat';
+import Web3 from 'web3';
+
+export interface AccountInfoStruct {
+  owner: string;
+  number: BigNumberish;
+}
+
+export interface AssetAmountStruct {
+  sign: boolean;
+  denomination: AmountDenomination;
+  ref: AmountReference;
+  value: BigNumberish;
+}
+
+export interface ActionArgsStruct {
+  actionType: BigNumberish;
+  accountId: BigNumberish;
+  amount: AssetAmountStruct;
+  primaryMarketId: BigNumberish;
+  secondaryMarketId: BigNumberish;
+  otherAddress: string;
+  otherAccountId: BigNumberish;
+  data: BytesLike;
+}
+
+export interface GenericTraderParamStruct {
+  traderType: BigNumberish;
+  makerAccountIndex: BigNumberish;
+  trader: string;
+  tradeData: BytesLike;
+}
+
+export interface ParStruct {
+  sign: boolean;
+  value: BigNumberish;
+}
+
+export interface WeiStruct {
+  sign: boolean;
+  value: BigNumberish;
+}
+
+export function calculateApr(
+  newValue: BigNumberish,
+  oldValue: BigNumberish,
+  durationDeltaSeconds: BigNumberish,
+): BigNumber {
+  const base = ethers.BigNumber.from('1000000000000000000');
+  const newValueBN = ethers.BigNumber.from(newValue);
+  const oldValueBN = ethers.BigNumber.from(oldValue);
+  return newValueBN.mul(base).div(oldValueBN).sub(base).mul(365 * 86400)
+    .div(durationDeltaSeconds);
+}
+
+export function calculateApy(
+  newValue: BigNumberish,
+  oldValue: BigNumberish,
+  durationDeltaSeconds: BigNumberish,
+): BigNumber {
+  const newValueBN = ethers.BigNumber.from(newValue);
+  const oldValueBN = ethers.BigNumber.from(oldValue);
+  const one = ethers.BigNumber.from('1000000000000000000');
+  return one.add(calculateApr(newValueBN, oldValueBN, durationDeltaSeconds).div(365))
+    .pow(365)
+    .mul(one)
+    .div(one.pow(365))
+    .sub(one);
+}
+
+export function formatNumber(n: BigNumberish): string {
+  const numberBN = ethers.BigNumber.from(n);
+  return Web3.utils.fromWei(numberBN.toString());
+}
+
+export async function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
